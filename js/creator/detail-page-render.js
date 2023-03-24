ae67aecf873b1fe8e322b9b92fd37f60b05e4a8d
EmirJavascript <orozbekovjob@gmail.com>
2023-03-24 17:41:00 +0600

    done but going to fix some bags

---
+++ js/creator/detail-page-render.js
@@ -0,0 +1,20 @@
+export const detailPageRendering = (data) => {
+  const h1 = document.querySelector('h1')
+  const tds = document.querySelectorAll('td')
+  
+  h1.textContent = data.name
+  tds[0].textContent = 'Жанры:'
+  tds[1].textContent = data.genres
+  tds[2].textContent = 'Автор:'
+  tds[3].textContent = data.author
+  tds[4].textContent = 'Год выпуска:'
+  tds[5].textContent = data.publishYear
+  tds[6].textContent = 'Язык оригинала:'
+  tds[7].textContent = data.originalLanguage
+  tds[8].textContent = 'Издательство:'
+  tds[9].textContent = data.publishHouse
+  tds[10].textContent = 'Колличество страниц:'
+  tds[11].textContent = data.pagesNumber
+
+  return data
+  }
